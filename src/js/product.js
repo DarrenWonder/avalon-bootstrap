87ce588fbd8741288e9decbb2e2e790f89bccb96
sunxiaoling <2535477912@qq.com>
2017-10-14 20:00:01 +0800

    私人订制首页  旅游产品首页

---
+++ src/js/product.js
@@ -0,0 +1,15 @@
+require(["product"], function (){
+    var dest = [];
+    var type = [];
+    function screenCondition (className) {
+        $(className).click(function(){
+            if ($(this).index() > 0) {
+                $(className).eq(0).removeClass('active-btn');
+                $(this).toggleClass('active-btn');
+            } else if ($(this).index() == 0){
+                $(this).addClass('active-btn');
+                $(this).siblings().removeClass('active-btn');
+            }
+        })
+    }
+});
