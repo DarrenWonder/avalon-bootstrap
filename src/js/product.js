40f597e55381ded7b33eb898eb47cfa8ed11ccf3
sunxiaoling <2535477912@qq.com>
2017-10-16 14:55:12 +0800

    私人订制首页  旅游产品首页

--- src/js/product.js
+++ src/js/product.js
@@ -2,11 +2,15 @@ require.config({
     paths: {
         jquery: 'https://cdn.bootcss.com/jquery/1.9.1/jquery.min',
         modernizr: 'https://cdn.bootcss.com/modernizr/2.8.3/modernizr.min',
-        domReady: 'https://cdn.bootcss.com/require-domReady/2.0.1/domReady.min'
+        domReady: 'https://cdn.bootcss.com/require-domReady/2.0.1/domReady.min',
+        avalon: 'vendor/avalon'
     },
     shim: {
     }
 });
-require(['jquery', 'type-check', "domReady!", "modernizr"], function($, check) {
+require(['jquery', 'avalon', 'type-check', 'domReady!', 'modernizr' ], function($, avalon, check) {
+
+
     check('.dest-list>li');
+    check('.type-list>li');
 });
--- src/js/type-check.js
+++ src/js/type-check.js
@@ -1,14 +1,37 @@
-define(['jquery'], function ($){
+define(['jquery', 'avalon'], function ($, avalon){
+    var vm = avalon.define({
+        $id: "test",
+        infoList: []
+    });
     var dest = [];
-    var type = [];
+    var type = 0;
     function screenCondition (className) {
         $(className).click(function(){
-            if ($(this).index() > 0) {
-                $(className).eq(0).removeClass('active-btn');
-                $(this).toggleClass('active-btn');
-            } else if ($(this).index() == 0){
+            var dataId = parseInt($(this).attr('data-id'));
+            if (className == '.dest-list>li') {
+                if ($(this).index() > 0) {
+                    $(className).eq(0).removeClass('active-btn');
+                    $(this).toggleClass('active-btn');
+                    dest.push(dataId);
+                } else if ($(this).index() == 0){
+                    $(this).addClass('active-btn');
+                    $(this).siblings().removeClass('active-btn');
+                    dest = [];
+                }
+            } else if ((className == '.type-list>li')){
                 $(this).addClass('active-btn');
                 $(this).siblings().removeClass('active-btn');
+                type = dataId
+            }
+        })
+        console.log(dest)
+        $.ajax({
+            type: 'GET',
+            url: 'http://devadmin.bmtrip.com/api/product/list?page=1&district_id='+dest+'&type='+type,
+            dataType: 'json',
+            async: false,
+            success: function (result) {
+                console.log(result)
             }
         })
     }
